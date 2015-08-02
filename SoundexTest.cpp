eed7bc3d25617d5dcf8b39694605d1f8d1f96fbc
qiubix <qiubix@gmail.com>
2015-08-02 21:19:23 +0200

    Cleanup after the first test

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -2,17 +2,18 @@
 
 class Soundex {
 public:
-  std::string encode(const std::string& name) const {
-    return "A";
-  };
+  std::string encode(const std::string& word) const {
+    return word;
+  }
 };
 
 #include "gmock/gmock.h"
+using ::testing::Eq;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
   Soundex soundex;
 
   auto encoded = soundex.encode("A");
 
-  ASSERT_THAT(encoded, testing::Eq("A"));
+  ASSERT_THAT(encoded, Eq("A"));
 }
