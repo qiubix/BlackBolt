02c7cfa8fe2bfd76d50c07fc912ab8ab1ec26f04
qiubix <qiubix@gmail.com>
2015-08-10 12:20:58 +0200

    Combine duplicate encodings

--- Soundex.h
+++ Soundex.h
@@ -12,6 +12,19 @@ public:
     return zeroPad(head(word) + encodedDigits(tail(word)));
   }
 
+  std::string encodedDigit(char letter) const {
+    const std::unordered_map<char, std::string> encodings {
+      {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+      {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+      {'d', "3"}, {'t', "3"},
+      {'l', "4"},
+      {'m', "5"}, {'n', "5"},
+      {'r', "6"}
+    };
+    auto it = encodings.find(letter);
+    return it == encodings.end() ? "" : it->second;
+  }
+
 private:
   std::string head(const std::string& word) const {
     return word.substr(0, 1);
@@ -25,26 +38,19 @@ private:
     std::string encoding;
     for (auto letter : word) {
       if (isComplete(encoding)) break;
-      encoding += encodedDigit(letter);
+      if (encodedDigit(letter) != lastDigit(encoding))
+        encoding += encodedDigit(letter);
     }
     return encoding;
   }
 
   bool isComplete(const std::string& encoding) const {
-      return encoding.length() == MaxCodeLength - 1;
+    return encoding.length() == MaxCodeLength - 1;
   }
 
-  std::string encodedDigit(char letter) const {
-    const std::unordered_map<char, std::string> encodings {
-      {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
-      {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
-      {'d', "3"}, {'t', "3"},
-      {'l', "4"},
-      {'m', "5"}, {'n', "5"},
-      {'r', "6"}
-    };
-    auto it = encodings.find(letter);
-    return it == encodings.end() ? "" : it->second;
+  std::string lastDigit(const std::string& encoding) const {
+    if (encoding.empty()) return "";
+    return std::string(1,encoding.back());
   }
 
   std::string zeroPad(const std::string& word) const {
